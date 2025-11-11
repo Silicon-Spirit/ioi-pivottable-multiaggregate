f45dd4e436b20774ecdec84592f84af553fbe5a3
luxapientia <trongtruong.pham192@gmail.com>
2025-11-12 04:00:33 +0700

    build: configure vue compile flags

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+	define: {
+		__VUE_OPTIONS_API__: true,
+		__VUE_PROD_DEVTOOLS__: false,
+		__VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+	},
 	server: {
 		port: 5173,
 		open: true,
